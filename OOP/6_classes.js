24c69fd97a7b34461341f3b6bbfdc791c628d5b0
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-14 18:17:19 +0100

    classes

---
+++ OOP/6_classes.js
@@ -0,0 +1,34 @@
+class Book {
+    constructor(title, author, year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    getSummary(){
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    }
+
+    getAge(){
+        const years = new Date().getFullYear() - this.year
+    return `${this.title} is ${years} years old`;
+    }
+
+    revise(newYear){
+        this.year = newYear;
+        this.revised = true;
+    }
+
+    static topBookStore(){
+        return 'Barnes & Noble';
+    }
+}
+
+// Instantiate Object
+const book1 = new Book('Book One', 'John Matheo', '2015');
+
+// console.log(book1);
+// book1.revise('2019');
+// console.log(book1);
+
+console.log(Book.topBookStore());
