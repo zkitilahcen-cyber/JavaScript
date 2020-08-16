ebfaa08aa411e1c1b766b9b37a69cfe28dc7f633
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-16 13:30:43 +0100

    filter

--- Higher Order Functions & Arrays/main.js
+++ Higher Order Functions & Arrays/main.js
@@ -17,10 +17,63 @@ const ages = [33, 12, 20, 16, 5, 21, 54, 21, 44, 61, 13, 45, 15, 25, 64, 32];
 // }
 
 // forEach
-companies.forEach(function(company){
-    console.log(company.category);
-})
+
+// companies.forEach(function(company){
+//     console.log(company.category);
+// })
+
 // filter
+
+// Get 21 and  older
+
+// let canDrink = [];
+// for(let i = 0; i<ages.length; i++){
+//     if(ages[i]>= 21){
+//         canDrink.push(ages[i]);
+//     }
+// }
+
+// const canDrink = ages.filter(function(age){
+//     if(age >= 21){
+//         return true;
+//     }
+// });
+
+// const canDrink = ages.filter(age => age >= 21);
+ 
+// console.log(canDrink)
+
+// Filter retail companies
+
+// const retailCompanies = companies.filter(function(company){
+//     if(company.category === 'Retail'){
+//         return true;
+//     }
+// })
+
+// const retailCompanies = companies.filter(company => company.category === 'Auto');
+
+// console.log(retailCompanies);
+
+// Get 80s companies
+
+// const eightiesCompanies = companies.filter(function(company){
+//     if(company.start >=1980 && company.start <1990){
+//         return true;
+//     }
+// });
+
+// const eightiesCompanies = companies.filter(company => (company.start >=1980 && company.start <1990))
+
+// console.log(eightiesCompanies);
+
+// Get companies that lasted 10 years
+ const lastedTenYearsCompanies = companies.filter(company => (company.end - company.start) >= 10);
+
+ console.log(lastedTenYearsCompanies);
+
+
+
 // map
 // sort
 // reduce
