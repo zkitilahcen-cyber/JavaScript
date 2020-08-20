012ed73de4a7598fb272e2b8d073ba7c0a7a5138
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-20 19:43:27 +0100

    connected with db

--- AJAX/process.php
+++ AJAX/process.php
@@ -1,11 +1,23 @@
 <?php
 
+    // Connect to a database
+    $conn = mysqli_connect('localhost','root', '1234', 'AjaxTest');
+
     echo 'Processing...';
 
     
       // Check for POST variable
       if(isset($_POST['name'])){
+        $name = mysqli_real_escape_string($conn, $_POST['name']);
         echo 'POST: Your name is '. $_POST['name'];
+
+        $query = "INSERT INTO users(name) VALUES('$name')";
+
+        if(mysqli_query($conn, $query)){
+            echo ' User Added...';
+        } else {
+            echo 'ERROR: '.mysqli_error($conn);
+        }
     }
     
     // Check for GET variable
