70a1f7e5225ca4309708b312c1089a4657495826
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-20 13:27:58 +0100

    POST with PHP

--- AJAX/process.php
+++ AJAX/process.php
@@ -2,6 +2,12 @@
 
     echo 'Processing...';
 
+    
+      // Check for POST variable
+      if(isset($_POST['name'])){
+        echo 'POST: Your name is '. $_POST['name'];
+    }
+    
     // Check for GET variable
     if(isset($_GET['name'])){
         echo 'GET: Your name is '. $_GET['name'];
