14b13c795eca1dfe0a750b6a629b24f65982b187
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-25 09:52:09 +0100

    get json data

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -4,13 +4,24 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
    if(req.url === '/'){
-    fs.readFile(
-        path.join(__dirname, 'public', 'index.html'), (err, content) => {
-            if(err) throw err;
-            res.writeHead(200, {'Content-Type':'text/html'});
-            res.end(content);
-    });
+        fs.readFile(
+            path.join(__dirname, 'public', 'index.html'), 
+            (err, content) => {
+                if(err) throw err;
+                res.writeHead(200, {'Content-Type':'text/html'});
+                res.end(content);
+            }
+        );
    }
+
+   if(req.url === '/api/users'){
+        const users = [
+            {name: 'Zkiti', age:27},
+            {name: 'Lahcen', age:28}
+        ];
+        res.writeHead(200, {'Content-Type':'application/json'});
+        res.end(JSON.stringify(users));
+    }
 });
 
 const PORT = process.env.PORT || 5000;
