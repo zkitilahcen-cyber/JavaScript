79e0d4199db33ddecc893a6cbd5f05ec090e70f5
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-26 19:43:25 +0100

    added router file

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -1,24 +1,17 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Members');
-const logger = require('./middleware/logger');
-const { filter } = require('./Members');
 
 
 const app = express();
 
-
-
 // Init middleware
 // app.use(logger);
-
-// Get all Members
-app.get('/api/members', (req, res) =>   res.json(members));
-
 
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
