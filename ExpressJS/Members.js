b1bb0739f5dda1d7ade09129bc3b9b04fcb2e97e
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-26 18:50:52 +0100

    added members.js

---
+++ ExpressJS/Members.js
@@ -0,0 +1,34 @@
+const members = [
+    {
+        id:1,
+        name: 'lahcen',
+        email: 'lahcen@gmail.com',
+        status: 'active'
+    },
+    {
+        id:2,
+        name: 'joee',
+        email: 'joee@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id:3,
+        name: 'lan',
+        email: 'lan@gmail.com',
+        status: 'active'
+    },
+    {
+        id:4,
+        name: 'franc',
+        email: 'franc@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id:5,
+        name: 'Bob',
+        email: 'bob@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
