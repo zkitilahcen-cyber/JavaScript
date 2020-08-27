2f19c445031ad28f7381c8f3634588972e0c9051
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-27 13:44:17 +0100

    added Body Parser Middleware

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -7,10 +7,14 @@ const app = express();
 // Init middleware
 // app.use(logger);
 
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Members API Routes
 app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
