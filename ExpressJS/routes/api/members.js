ebb92c423dd186ab36ebfe21c3b542b71cba0508
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-27 13:43:35 +0100

    post member

--- ExpressJS/routes/api/members.js
+++ ExpressJS/routes/api/members.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const members = require('../../Members');
+const uuid = require('uuid');
 
 
 // Get all Members
@@ -15,5 +16,22 @@ router.get('/:id', (req, res) => {
         res.status(400).json({msg: `No member with id of ${req.params.id}`});
     }
 });
+
+// Create Member
+router.post('/', (req, res) => {
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    };
+
+    if(!newMember.name || !newMember.email) {
+        return res.status(400).json({message: 'Please include a name and email'});
+    }
+
+    members.push(newMember);
+    res.json(members);
+})
 
 module.exports = router;
